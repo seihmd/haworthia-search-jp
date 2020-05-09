10b8504d2c2f2fcbc6c749c43a4b61813b0d3f9f
seihmd <7434823+seihmd@users.noreply.github.com>
2020-05-09 21:42:40 +0900

    japanese search

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import data from "./data/vol1-1.json";
 
-const index = new FlexSearch();
+const jaIndex = FlexSearch.create({
+    encode: false,
+    tokenize: function (str) {
+        return str.replace(/[\x00-\x7F]/g, "").split("");
+    }
+});
+const enIndex = FlexSearch.create({
+    tokenize: 'full',
+    threshold: 2
+});
+
+const listMax = 20;
 
 const resultContainer = document.querySelector('#resultContainer');
 
 for (const id in data) {
-    if (data.hasOwnProperty(id)) {
-        const item = data[id];
-        index.add(id, item.name);
-        index.add(id, item.name_en);
-    }
+    const item = data[id];
+    jaIndex.add(id, item.name);
+    enIndex.add(id, item.name_en);
 }
 
 document.querySelector("#searchText").addEventListener("input", (e) => {
-    const results = index.search(e.target.value, 10);
+    const inputs = e.target.value.replace('　', ' ').split(' ');
+    let results = jaIndex.search(e.target.value, listMax);
+    if (results.length < listMax) {
+        results = results.concat(
+            enIndex.search(e.target.value, listMax - results.length)
+        );
+    }
 
     const htmls = results.map((id) => {
         const item = data[id];
